2f49c5c35fb7c2a2122d15f5d28302397c9688dd
f-akhtar <fakhtar@gitam.in>
2023-12-31 03:02:06 +0530

    Intial Commit

--- script.js
+++ script.js
@@ -20,3 +20,41 @@ document.querySelector('.fas.fa-bars').addEventListener('click', function () {
 document.querySelector('.fas.fa-times').addEventListener('click', function () {
     document.querySelector('nav ul').classList.remove('show');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the button elements
+    var btnShowMore = document.querySelector('.btn-show-more');
+    var btnShowLess = document.querySelector('.btn-show-less');
+
+    // Add click event listeners
+    btnShowMore.addEventListener('click', function (event) {
+        // Show the next four works
+        var hiddenWorks = document.querySelectorAll('.work-list .work:nth-child(n+5):not([style*="display: block"])');
+        for (var i = 0; i < hiddenWorks.length; i++) {
+            hiddenWorks[i].style.display = 'block';
+        }
+
+        // Toggle button visibility
+        btnShowMore.style.display = 'none';
+        btnShowLess.style.display = 'block';
+
+        // Prevent default anchor behavior
+        event.preventDefault();
+    });
+
+    btnShowLess.addEventListener('click', function (event) {
+        // Hide works beyond the initial four
+        var hiddenWorks = document.querySelectorAll('.work-list .work:nth-child(n+5)');
+        for (var i = 0; i < hiddenWorks.length; i++) {
+            hiddenWorks[i].style.display = 'none';
+        }
+
+        // Toggle button visibility
+        btnShowMore.style.display = 'block';
+        btnShowLess.style.display = 'none';
+
+        // Prevent default anchor behavior
+        event.preventDefault();
+    });
+});
+
