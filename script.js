392397b7c872d7eade6a36b3a5ce25665ea5dd91
f-akhtar <fakhtar@gitam.in>
2024-01-01 00:50:21 +0530

    Final Launch

--- script.js
+++ script.js
@@ -58,3 +58,27 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+var sidemeu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemeu.style.right = "0";
+}
+function closemenu(){
+    sidemeu.style.right = "-200px";
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyyX3gLoIJz0GJgiikui5O7oKeutYTCdlbXMj1cLEkHMwDJyv0n4n60cQ0ESBVcxYW6iQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message Send Successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
